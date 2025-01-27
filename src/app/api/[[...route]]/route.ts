b32e61f888580df35b5db03faef8a30ef58d6151
Alejandro Chinchilla <alesteller8@gmail.com>
2025-01-26 18:11:42 -0600

    feat: add healthcheck endpoint, update trigger project ID

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,22 +1,36 @@
-import { zValidator } from "@hono/zod-validator";
+import db from "@/server/db";
+import {
+  dispatchedStations,
+  dispatchedVehicles,
+  incidentTypes,
+  incidents,
+  stations,
+  vehicleDisponibility,
+  vehicles
+} from "@/server/db/schema";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import z from "zod";
 
 const app = new Hono().basePath("/api");
 
-app.post(
-  "/sync",
-  zValidator(
-    "form",
-    z.object({
-      token: z.coerce.string()
-    })
-  ),
-  (c) => {
-    return c.newResponse("OK");
-  }
-);
+app.get("/healthcheck", async (c) => {
+  const vehiclesCount = await db.$count(vehicles);
+  const vehicleDisponibilityCount = await db.$count(vehicleDisponibility);
+  const vehiclesDispatchedCount = await db.$count(dispatchedVehicles);
+  const incidentTypesCount = await db.$count(incidentTypes);
+  const incidentsCount = await db.$count(incidents);
+  const stationsCount = await db.$count(stations);
+  const dispatchedStationCount = await db.$count(dispatchedStations);
+
+  return c.json({
+    vehiclesCount,
+    vehicleDisponibilityCount,
+    vehiclesDispatchedCount,
+    incidentTypesCount,
+    incidentsCount,
+    stationsCount,
+    dispatchedStationCount
+  });
+});
 
 export const GET = handle(app);
-export const POST = handle(app);
