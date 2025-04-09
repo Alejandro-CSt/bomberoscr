bb956dc10b0341aba220f1a157f54eea22ab3d12
Alejandro Chinchilla <alesteller8@gmail.com>
2025-04-09 02:48:13 -0600

    add dynamic sitemap generation

---
+++ apps/web/src/app/sitemap.ts
@@ -0,0 +1,33 @@
+import env from "@/server/env";
+import db from "@bomberoscr/db/db";
+import { stations as stationsSchema } from "@bomberoscr/db/schema";
+import { eq } from "drizzle-orm";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const siteUrl = env.SITE_URL || "https://bomberos.anifz.com";
+  const stations = await db.query.stations.findMany({
+    columns: {
+      name: true
+    },
+    where: eq(stationsSchema.isOperative, true)
+  });
+
+  return [
+    {
+      url: siteUrl,
+      lastModified: new Date(),
+      priority: 1
+    },
+    {
+      url: `${siteUrl}/incidentes`,
+      lastModified: new Date(),
+      changeFrequency: "always"
+    },
+    ...stations.map((station) => ({
+      url: `${siteUrl}/estaciones/${station.name}`,
+      lastModified: new Date(),
+      changeFrequency: "hourly" as const
+    }))
+  ];
+}
