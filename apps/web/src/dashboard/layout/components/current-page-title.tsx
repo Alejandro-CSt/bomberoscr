d88233fa366e6159f0b5b43d2aa6289f0655af27
Alejandro Chinchilla <alesteller8@gmail.com>
2025-04-15 19:38:29 -0600

    feat(dashboard): replace title rendering with CurrentPageTitle component

---
+++ apps/web/src/dashboard/layout/components/current-page-title.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { destinations } from "@/dashboard/layout/components/sidebar";
+import { usePathname } from "next/navigation";
+
+export function CurrentPageTitle() {
+  const path = usePathname();
+  const activeDestination = destinations.navMain.find((item) => path.startsWith(item.url));
+
+  return (
+    <h1 className="font-medium text-base">
+      {activeDestination ? activeDestination.title : "Estadísticas"}
+    </h1>
+  );
+}
