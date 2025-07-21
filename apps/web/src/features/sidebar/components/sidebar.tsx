14b83a35f653d54479f4f41adbb2e474527c840f
Alejandro Chinchilla <alesteller8@gmail.com>
2025-07-20 20:41:40 -0600

    change logo bg to red-600

--- apps/web/src/features/sidebar/components/sidebar.tsx
+++ apps/web/src/features/sidebar/components/sidebar.tsx
@@ -58,7 +58,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuItem>
               <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
                 <Link href="/">
-                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-red-600 text-sidebar-primary-foreground">
                     <FireTruckIcon className="size-4" weight="fill" />
                   </div>
                 </Link>
