2a2f97bf098afe1e9c1f46871b676ae3373c25fd
Alejandro Chinchilla <alesteller8@gmail.com>
2025-08-29 13:03:19 -0600

    refactor(web): update /incidentes loading state to match new page [skip ci]

--- apps/web/src/app/(dashboard)/incidentes/loading.tsx
+++ apps/web/src/app/(dashboard)/incidentes/loading.tsx
@@ -1,54 +1,55 @@
-import { FloatingPanelHeader } from "@/map/layout/components/floating-panel-header";
-import { Select, SelectContent, SelectTrigger, SelectValue } from "@/shared/components/ui/select";
+import { IsOpenIndicatorLegend } from "@/features/incidents/table/components/data-table-is-open-column";
+import { Button } from "@/shared/components/ui/button";
+import { Input } from "@/shared/components/ui/input";
 import { Skeleton } from "@/shared/components/ui/skeleton";
+import { FilterIcon, SearchIcon, SlidersHorizontalIcon } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="flex h-full flex-col overflow-y-auto">
-      <FloatingPanelHeader title="Incidentes" />
-      <div className="flex flex-col gap-4 p-4">
-        <div className="flex items-center">
-          <span className="mr-2 text-muted-foreground text-sm">Filtrar</span>
-          <Select disabled defaultValue="all">
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Todas las estaciones" />
-            </SelectTrigger>
-            <SelectContent>
-              <div />
-            </SelectContent>
-          </Select>
+    <div className="flex h-full flex-col p-4">
+      <div className="flex items-center justify-between gap-6 bg-background pb-2">
+        <div className="flex items-stretch overflow-hidden rounded-lg border">
+          <div className="flex items-center justify-center border-r px-3 py-2">
+            <SearchIcon className="size-4" />
+          </div>
+          <Input
+            placeholder="Buscar incidente"
+            className="flex-1 border-0 px-3 py-2 outline-0 ring-0 focus-visible:ring-0"
+          />
+          <div className="flex cursor-pointer items-center justify-center border-l px-3 py-2 hover:bg-muted/50">
+            <FilterIcon className="size-4" />
+          </div>
         </div>
-        <div className="mb-8 flex flex-col gap-2 md:overflow-y-auto">
-          {Array(5)
-            .fill(0)
-            .map((_, i) => (
-              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-              <div key={i} className="rounded-lg border p-4">
-                <div className="flex justify-between gap-2">
-                  <div className="flex flex-col">
-                    <Skeleton className="mb-1 h-5 w-36 sm:w-48" />
-                    <Skeleton className="h-4 w-28 sm:w-32" />
-                  </div>
-                  <div className="flex shrink-0 flex-col items-start gap-1 sm:items-end">
-                    <Skeleton className="h-5 w-20 sm:w-24" />
-                    <div className="flex gap-2">
-                      <Skeleton className="h-5 w-16 sm:w-20" />
-                      <Skeleton className="h-5 w-16 sm:w-20" />
-                    </div>
-                  </div>
-                </div>
+        <Button variant="outline" size="icon">
+          <SlidersHorizontalIcon />
+        </Button>
+      </div>
 
-                <div className="mt-3">
-                  <Skeleton className="h-4 w-full" />
-                  <Skeleton className="mt-1 h-4 w-4/5" />
-                </div>
+      <IsOpenIndicatorLegend className="py-2 text-muted-foreground" />
 
-                <div className="mt-3 flex flex-col">
-                  <Skeleton className="h-3 w-20" />
-                  <Skeleton className="mt-1 h-4 w-36" />
+      <div className="flex-1 overflow-hidden">
+        <div className="border border-border md:border-r md:border-l">
+          <div className="grid grid-cols-5 gap-4 border-b p-3">
+            <Skeleton className="h-5 w-10" />
+            <Skeleton className="h-5 w-24" />
+            <Skeleton className="h-5 w-28" />
+            <Skeleton className="h-5 w-40" />
+            <Skeleton className="h-5 w-48" />
+          </div>
+          <div className="divide-y">
+            {Array(35)
+              .fill(0)
+              .map((_, i) => (
+                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                <div key={i} className="grid grid-cols-5 items-center gap-4 p-3">
+                  <Skeleton className="h-4 w-6" />
+                  <Skeleton className="h-4 w-24" />
+                  <Skeleton className="h-4 w-28" />
+                  <Skeleton className="h-4 w-40" />
+                  <Skeleton className="h-4 w-48" />
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
       </div>
     </div>
