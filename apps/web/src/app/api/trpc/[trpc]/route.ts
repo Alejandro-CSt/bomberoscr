327422ea16cbe1b30d9bae67a56b321901db9e13
Alejandro Chinchilla <alesteller8@gmail.com>
2025-09-08 01:18:44 -0600

    fix: update TRPC endpoint

--- apps/web/src/app/api/trpc/[trpc]/route.ts
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -3,7 +3,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
-    endpoint: "/api/trpc",
+    endpoint: process.env.NODE_ENV === "production" ? "/bomberos/api/trpc" : "/api/trpc",
     req,
     router: appRouter,
     createContext: () => ({})
--- apps/web/src/features/trpc/provider.tsx
+++ apps/web/src/features/trpc/provider.tsx
@@ -11,7 +11,7 @@ export default function TRPCProvider({ children }: { children: React.ReactNode }
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "/api/trpc"
+          url: process.env.NODE_ENV === "production" ? "/bomberos/api/trpc" : "/api/trpc"
         })
       ]
     })
